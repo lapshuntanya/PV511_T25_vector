91f081798ea532d4b4caf3229bf99b0d8e81f575
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-04-20 19:04:07 +0300

    MyVector push_back

--- MyVector.h
+++ MyVector.h
@@ -1,19 +1,57 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 template<typename Type>
-class MyVector {
+class MyVector { //vector
     Type* arr;
     int size;
 
 public:
-    MyVector();
-    MyVector(int size, Type value);
-    ~MyVector();
+    MyVector() {//size = 0; []
+        arr = nullptr;
+        size = 0;
+    }
+    MyVector(int size, Type value) {//size = 3, value = 0,  [0, 0, 0]
+        this->size = size;
+        arr = new Type[size];
+        for (int i =0; i < size; i++) {
+            arr[i] = value;
+        }
+    }
+    ~MyVector() {
+        if (arr!= nullptr) {
+            delete[] arr;
+        }
+        arr = nullptr;
+        size = 0;
+    }
 
-    int getSize() const;	// повертає розмірність масиву
-    void print()const;	// виведення всіх елементів масиву
+    int getSize() const	// повертає розмірність масиву
+    {
+        return size;
+    }
+    void  print()const	// виведення всіх елементів масиву
+    {
+        for (int i =0; i < size; i++) {
+            cout << arr[i] << " ";
+           // printf("%d ", arr[i]);
+        }
+        cout << endl;
+    }
 
-    void push_back(Type item);	// додавання елемента item у кінець масиву
+    void push_back(Type item)	// додавання елемента item у кінець масиву
+    {
+        Type* tmp = new Type[size + 1];
+        for (int i =0 ; i < size; i++) {
+            tmp[i] = arr[i];
+        }
+        delete[] arr;
+        size++;
+        tmp[size -1] = item;
+        arr = tmp;
+    }
+
     Type& operator[](int number);
 
     Type find_max()const; // пошук елемента з максимальним значенням
--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 
 int main() {
 
+    if () {
+        MyVector<int> a(3, 0); //[0, 0 , 0]
+        //
+        ...
+        a.getsize();// size = 3
+        //
+    }//~MyVector
 
-
+    a
     return 0;
 }
